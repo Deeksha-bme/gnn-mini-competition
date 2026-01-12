21f892bc401c3c642b915ed1368dc4d2bfe9600a
Deeksha-bme <deeksha.bme@gmail.com>
2026-01-12 11:34:51 -0500

    Create train.py

---
+++ train.py
@@ -0,0 +1,9 @@
+print("G4RS Mini-Competition")
+print("Baseline GNN experiment will be implemented here.")
+
+# TODO:
+# 1. Load dataset
+# 2. Define GNN model (GCN)
+# 3. Train model
+# 4. Evaluate on validation/test set
+# 5. Update leaderboard in README
